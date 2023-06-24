7ed361a268f359235eecbfc8dc164e32bc3492bd
sobbey99 <sobbey99@gmail.com>
2023-06-24 19:56:05 +0300

    loadingManager, sceen, cars, DRACO

---
+++ src/js/constants.js
@@ -0,0 +1,117 @@
+import { Path, Vector3 } from "yuka";
+
+const YELLOWVEHICLESPATHS = [];
+const BLUEVEHICLESPATHS = [];
+const REDVEHICLESPATHS = [];
+
+const yellowV1 = new Path();
+yellowV1.add(new Vector3(5.91, 0.3, 125.92));
+yellowV1.add(new Vector3(5.72, 0.3, 93.68));
+YELLOWVEHICLESPATHS.push(yellowV1);
+
+const yellowV2 = new Path();
+yellowV2.add(new Vector3(6.21, 0.3, 30.19));
+yellowV2.add(new Vector3(7.07, 0.3, 24.66));
+yellowV2.add(new Vector3(33.32, 0.3, 24.36));
+YELLOWVEHICLESPATHS.push(yellowV2);
+
+const yellowV3 = new Path();
+yellowV3.add(new Vector3(93.03, 0.3, 24.5));
+yellowV3.add(new Vector3(102, 0.3, 22.84));
+yellowV3.add(new Vector3(102.42, 0.3, -1.27));
+YELLOWVEHICLESPATHS.push(yellowV3);
+
+const yellowV4 = new Path();
+yellowV4.add(new Vector3(102.5, 0.3, -66));
+yellowV4.add(new Vector3(99.92, 0.3, -73.97));
+yellowV4.add(new Vector3(76.0, 0.3, -75.41));
+YELLOWVEHICLESPATHS.push(yellowV4);
+
+const yellowV5 = new Path();
+yellowV5.add(new Vector3(11.86, 0.3, -75.86));
+yellowV5.add(new Vector3(5.98, 0.3, -75.96));
+yellowV5.add(new Vector3(5.63, 0.3, -102.59));
+YELLOWVEHICLESPATHS.push(yellowV5);
+
+const yellowV6 = new Path();
+yellowV6.add(new Vector3(5.97, 0.3, -161.04));
+yellowV6.add(new Vector3(4.55, 0.3, -169.5));
+yellowV6.add(new Vector3(-20.11, 0.3, -170.21));
+YELLOWVEHICLESPATHS.push(yellowV6);
+
+const yellowV7 = new Path();
+yellowV7.add(new Vector3(-82.82, 0.3, -171.17));
+yellowV7.add(new Vector3(-115.08, 0.3, -170.5));
+YELLOWVEHICLESPATHS.push(yellowV7);
+
+//Red cars
+
+const redV1 = new Path();
+redV1.add(new Vector3(1.38, 0.3, 109.32));
+redV1.add(new Vector3(3.91, 0.3, 118.82));
+redV1.add(new Vector3(27.74, 0.3, 119.04));
+REDVEHICLESPATHS.push(redV1);
+
+const redV2 = new Path();
+redV2.add(new Vector3(1.13, 0.3, 14.01));
+redV2.add(new Vector3(3.7, 0.3, 22.64));
+redV2.add(new Vector3(26.53, 0.3, 24.73));
+REDVEHICLESPATHS.push(redV2);
+
+const redV3 = new Path();
+redV3.add(new Vector3(107.5, 0.3, 20.33));
+redV3.add(new Vector3(102.63, 0.3, 18.32));
+redV3.add(new Vector3(102.45, 0.3, -8.42));
+REDVEHICLESPATHS.push(redV3);
+
+const redV4 = new Path();
+redV4.add(new Vector3(97.45, 0.3, -81.35));
+redV4.add(new Vector3(97.98, 0.3, -50.34));
+REDVEHICLESPATHS.push(redV4);
+
+const redV5 = new Path();
+redV5.add(new Vector3(-3.55, 0.3, -71.24));
+redV5.add(new Vector3(5.51, 0.3, -73.1));
+redV5.add(new Vector3(6.15, 0.3, -97.01));
+REDVEHICLESPATHS.push(redV5);
+
+const redV6 = new Path();
+redV6.add(new Vector3(1.45, 0.3, -175.84));
+redV6.add(new Vector3(-0.64, 0.3, -170.2));
+redV6.add(new Vector3(-25.56, 0.3, -170.28));
+REDVEHICLESPATHS.push(redV6);
+
+const redV7 = new Path();
+redV7.add(new Vector3(-98.74, 0.3, -166.74));
+redV7.add(new Vector3(-67.84, 0.3, -166.61));
+REDVEHICLESPATHS.push(redV7);
+
+//Blue cars
+
+const blueV1 = new Path();
+blueV1.add(new Vector3(-3.55, 0.3, 119.5));
+blueV1.add(new Vector3(33.29, 0.3, 118.85));
+BLUEVEHICLESPATHS.push(blueV1);
+
+const blueV2 = new Path();
+blueV2.add(new Vector3(-4.08, 0.3, 24.64));
+blueV2.add(new Vector3(39.31, 0.3, 24.53));
+BLUEVEHICLESPATHS.push(blueV2);
+
+const blueV3 = new Path();
+blueV3.add(new Vector3(98.08, 0.3, 14.95));
+blueV3.add(new Vector3(98.53, 0.3, 45.91));
+BLUEVEHICLESPATHS.push(blueV3);
+
+const blueV4 = new Path();
+blueV4.add(new Vector3(93.599, 0.3, -70.83));
+blueV4.add(new Vector3(101.51, 0.3, -75.48));
+blueV4.add(new Vector3(102.25, 0.3, -96.45));
+BLUEVEHICLESPATHS.push(blueV4);
+
+const blueV7 = new Path();
+blueV7.add(new Vector3(-88.88, 0.3, -160.78));
+blueV7.add(new Vector3(-89, 0.3, -192.14));
+BLUEVEHICLESPATHS.push(blueV7);
+
+export { YELLOWVEHICLESPATHS, BLUEVEHICLESPATHS, REDVEHICLESPATHS };
---
+++ src/js/scripts.js
@@ -0,0 +1,186 @@
+import * as THREE from "three";
+import * as YUKA from "yuka";
+import * as gsap from "gsap";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import * as SkeletonUtils from "three/examples/jsm/utils/SkeletonUtils";
+import {
+  YELLOWVEHICLESPATHS,
+  BLUEVEHICLESPATHS,
+  REDVEHICLESPATHS,
+} from "./constants";
+
+const entityManager = new YUKA.EntityManager();
+const progressBar = document.getElementById("progress-bar");
+const progressBarContainer = document.querySelector(".progress-bar-container");
+const loadingManager = new THREE.LoadingManager();
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background
+renderer.setClearColor(0x94d8fb);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const ambientLight = new THREE.AmbientLight(0xe1e1e1, 0.3);
+scene.add(ambientLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0x94d8fb, 0x9cff2e, 0.3);
+scene.add(hemisphereLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+scene.add(directionalLight);
+
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+loadingManager.onProgress = (url, loaded, total) => {
+  progressBar.value = (loaded / total) * 100;
+};
+
+loadingManager.onLoad = () => {
+  progressBarContainer.style.display = "none";
+};
+
+const loader = new GLTFLoader(loadingManager);
+const dLoader = new DRACOLoader();
+dLoader.setDecoderPath(
+  "https://www.gstatic.com/draco/versioned/decoders/1.5.6/"
+);
+dLoader.setDecoderConfig({ type: "js" });
+loader.setDRACOLoader(dLoader);
+
+loader.load("./assets/terrain.glb", (glb) => {
+  const model = glb.scene;
+  scene.add(model);
+});
+
+// Camera positioning
+camera.position.set(3, 10, 218);
+camera.lookAt(scene.position);
+
+function sync(entity, renderComponent) {
+  renderComponent.matrix.copy(entity.worldMatrix);
+}
+
+function createCarV(model, path, entityManager, yRotation) {
+  const group = new THREE.Group();
+  scene.add(group);
+  group.matrixAutoUpdate = false;
+
+  const car = SkeletonUtils.clone(model);
+  group.add(car);
+
+  const v = new YUKA.Vehicle();
+  v.setRenderComponent(group, sync);
+
+  entityManager.add(v);
+
+  const followPathBehavior = new YUKA.FollowPathBehavior(path, 2);
+  const onPathBehavior = new YUKA.OnPathBehavior(path);
+  onPathBehavior.radius - 0.1;
+
+  v.position.copy(path.current());
+  v.maxSpeed = 5;
+  v.steering.add(onPathBehavior);
+  v.steering.add(followPathBehavior);
+
+  followPathBehavior.active = false;
+
+  v.rotation.fromEuler(0, yRotation, 0);
+
+  const vehicleAll = {
+    vehicle: v,
+    modelGroup: car,
+  };
+
+  return vehicleAll;
+}
+
+loader.load("./assets/SUV.glb", (glb) => {
+  const model = glb.scene;
+  const v1 = createCarV(model, YELLOWVEHICLESPATHS[0], entityManager, Math.PI);
+  const v2 = createCarV(model, YELLOWVEHICLESPATHS[1], entityManager, Math.PI);
+  const v3 = createCarV(
+    model,
+    YELLOWVEHICLESPATHS[2],
+    entityManager,
+    Math.PI / 2
+  );
+  const v4 = createCarV(model, YELLOWVEHICLESPATHS[3], entityManager, Math.PI);
+  const v5 = createCarV(
+    model,
+    YELLOWVEHICLESPATHS[4],
+    entityManager,
+    -Math.PI / 2
+  );
+  const v6 = createCarV(model, YELLOWVEHICLESPATHS[5], entityManager, Math.PI);
+  const v7 = createCarV(
+    model,
+    YELLOWVEHICLESPATHS[6],
+    entityManager,
+    -Math.PI / 2
+  );
+});
+
+loader.load("./assets/red.glb", (glb) => {
+  const model = glb.scene;
+  const v1 = createCarV(model, REDVEHICLESPATHS[0], entityManager, 0);
+  const v2 = createCarV(model, REDVEHICLESPATHS[1], entityManager, 0);
+  const v3 = createCarV(
+    model,
+    REDVEHICLESPATHS[2],
+    entityManager,
+    -Math.PI / 2
+  );
+  const v4 = createCarV(model, REDVEHICLESPATHS[3], entityManager, 0);
+  const v5 = createCarV(model, REDVEHICLESPATHS[4], entityManager, Math.PI / 2);
+  const v6 = createCarV(model, REDVEHICLESPATHS[5], entityManager, 0);
+  const v7 = createCarV(model, REDVEHICLESPATHS[6], entityManager, Math.PI / 2);
+});
+loader.load("./assets/blue.glb", (glb) => {
+  const model = glb.scene;
+  const v1 = createCarV(
+    model,
+    BLUEVEHICLESPATHS[0],
+    entityManager,
+    Math.PI / 2
+  );
+  const v2 = createCarV(
+    model,
+    BLUEVEHICLESPATHS[1],
+    entityManager,
+    Math.PI / 2
+  );
+  const v3 = createCarV(model, BLUEVEHICLESPATHS[2], entityManager, 0);
+  const v4 = createCarV(
+    model,
+    BLUEVEHICLESPATHS[3],
+    entityManager,
+    Math.PI / 2
+  );
+  const v7 = createCarV(model, BLUEVEHICLESPATHS[4], entityManager, Math.PI);
+});
+
+const time = new YUKA.Time();
+
+function animate() {
+  const delta = time.update().getDelta();
+  entityManager.update(delta);
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
